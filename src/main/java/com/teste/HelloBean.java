9948e6f4606658823228e3f51e3b1bdff03622eb
Everton Barbosa <104376567+nbeverton@users.noreply.github.com>
2023-09-14 21:58:58 -0300

    first commit

---
+++ src/main/java/com/teste/HelloBean.java
@@ -0,0 +1,22 @@
+package com.teste;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean(name="user")
+@SessionScoped
+public class HelloBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private String testeJSF;
+
+    public String getTesteJSF() {
+        return testeJSF;
+    }
+
+    public void setTesteJSF(String testeJSF) {
+        this.testeJSF = testeJSF;
+    }
+}
