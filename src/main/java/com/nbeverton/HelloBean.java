90f690afad3bbcbf082da6e9e5b2d314df864906
Everton Barbosa <104376567+nbeverton@users.noreply.github.com>
2023-09-18 22:15:45 -0300

    config.
    Configuração do xhtml. - Funcionando ok.

--- src/main/java/com/nbeverton/HelloBean.java
+++ src/main/java/com/nbeverton/HelloBean.java
@@ -1,4 +1,4 @@
-package com.teste;
+package com.nbeverton;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
---
+++ src/main/java/com/nbeverton/Person.java
@@ -0,0 +1,72 @@
+package com.nbeverton;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean
+public class Person {
+	
+	private int id;
+	private String name;
+	private String type;
+	private String email;
+	private String telephone;
+	
+	private String mensagem;
+	
+	public void mensagemOla() {
+		mensagem = "Olá, " + name;
+	}
+	
+	public Person() {
+    }
+
+    public Person(String name, String type, String email, String telephone) {
+        super();
+        this.name = name;
+        this.type = type;
+        this.email = email;
+        this.telephone = telephone;
+    }
+	
+	public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    
+    public String getMensagem() {
+		return mensagem;
+	}
+    
+    public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+}
